f164c932d2be483b244b93b91541e3252406942d
Lucas Capdevila <lcapdevila60@gmail.com>
2025-11-06 17:48:55 -0300

    Se agrega ruta pública Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Sidebar from "./components/common/Sidebar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
 import Footer from "./components/common/Footer";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div className="mx-auto">Home</div>
+  )
+}
+
+export default Home
