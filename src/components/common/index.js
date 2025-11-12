6d4cb8e8c194c1fb371bf9c71d8fabffcd4d11d6
Lucas Capdevila <lcapdevila60@gmail.com>
2025-11-12 10:48:12 -0300

    Se agrega hook para manejo de horarios de recorridos directos o con trasbordo

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,3 @@
 export { default as Footer } from "./Footer";
 export { default as Sidebar } from "./Sidebar";
-export { default as AdminTable } from "./AdminTable";
+export { default as AdminTable } from "../admin/AdminTable";
