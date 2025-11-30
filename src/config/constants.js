f4833294deb23b59474dfcc946ec27af55882f71
Lucas Capdevila <lcapdevila60@gmail.com>
2025-11-30 19:48:47 -0300

    Se realiza limpieza de imports

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+// Constantes de estilos compartidos
+export const COLORS = {
+  PRIMARY: '#0c5392',
+  WHITE: '#fff',
+};
+
+export const BUTTON_STYLES = {
+  PRIMARY: {
+    backgroundColor: COLORS.PRIMARY,
+    color: COLORS.WHITE,
+  },
+};
+
+export const PAGE_SIZE_DEFAULT = 10;
